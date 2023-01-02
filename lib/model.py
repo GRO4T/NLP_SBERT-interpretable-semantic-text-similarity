8841231ce48da27994962c1b4dbd7326cb405d3c
Damian Kolaska <damian.kolaska99@gmail.com>
2023-01-02 17:31:09 +0100

    Model implementation

---
+++ lib/model.py
@@ -0,0 +1,65 @@
+from pytorch_lightning import LightningModule
+from sentence_transformers import SentenceTransformer
+import torch
+import numpy as np
+
+from lib.utils import TYPES_MAP
+
+
+class SBERT_iSTS_Model(LightningModule):
+    def __init__(
+        self, sbert_model: str = "all-mpnet-base-v2", learning_rate: float = 0.001
+    ):
+        super().__init__()
+        self._sbert = SentenceTransformer(sbert_model)
+        # kolasdam(TODO) sparametryzować
+        self._scoring_head = torch.nn.Linear(in_features=384 * 2, out_features=1)
+        self._class_head = torch.nn.Linear(
+            in_features=384 * 2, out_features=len(TYPES_MAP)
+        )
+        self._learning_rate = learning_rate
+        self.save_hyperparameters()
+    
+    def _step(self, batch, batch_idx, id: str):
+        x, y = batch
+        y_hat = self.forward(x)
+        return self.loss(y, y_hat, id)
+
+    def forward(self, x):
+        embedding_a = self._sbert.encode(x[0])
+        embedding_b = self._sbert.encode(x[1])
+
+        ists_head_input = np.concatenate((embedding_a, embedding_b))
+
+        score = torch.reshape(self._scoring_head(ists_head_input), (-1,))
+        cls = torch.nn.functional.softmax(self._class_head(ists_head_input), dim=1)
+
+        return cls, score
+
+    def loss(self, y, y_hat, id):
+        scoring_loss = torch.nn.functional.mse_loss(y_hat[1], y[1])
+        class_loss = torch.nn.functional.binary_cross_entropy_with_logits(y_hat[0], y[0])
+        return scoring_loss + class_loss
+
+    def training_step(self, batch, batch_idx):
+        loss = self._step(batch, batch_idx, "train")
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        loss = self._step(batch, batch_idx, "val")
+        return loss
+
+    def test_step(self, batch, batch_idx):
+        loss = self._step(batch, batch_idx, "test")
+        return loss
+
+    def predict_step(self, batch, batch_idx):
+        x, y = batch
+        types, scores = self.forward(x)
+
+        return \
+            torch.argmax(types, dim=1), \
+            torch.clamp(torch.round(scores).int(), min=0, max=5)
+    
+    def configure_optimizers(self):
+        return torch.optim.AdamW(self.parameters(), lr=self._learning_rate)
--- tests/test_sbert_lib.py
+++ tests/test_sbert_lib.py
@@ -2,6 +2,8 @@ import unittest
 
 from torch import tensor
 
+import numpy as np
+
 from lib.dataset import SBERTDataset
 from lib.data_module import SBERTDataModule
 
@@ -27,3 +29,16 @@ class Test_SBERT_iSTS(unittest.TestCase):
         data_module = SBERTDataModule(
             train_path, test_path, batch_size=16, train_batch_size=16, num_workers=2
         )
+    
+    def test_numpy_concatenate(self):
+        a = np.array([1, 2, 3])
+        b = np.array([4, 5, 6])
+
+        self.assertIsInstance(a, np.ndarray)
+        self.assertIsInstance(b, np.ndarray)
+        
+        c = np.concatenate((a, b))
+
+        expected_c = np.array([1, 2, 3, 4, 5, 6])
+
+        self.assertEqual(c.all(), expected_c.all())
